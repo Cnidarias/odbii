d92bd16f1a0e797bb09cc34f84287a9cda9f4d49
roessner <pythonmolurus95@gmail.com>
2015-07-16 15:30:08 +0200

    working on gui component

--- gui.py
+++ gui.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import time
+import random
+
 import subprocess
 from multiprocessing.connection import Listener
 
 from PyQt4 import QtGui as qt
+from PyQt4 import QtCore as qtc
 import pyqtgraph as pg
 
 
@@ -36,31 +39,159 @@ import pyqtgraph as pg
 #
 
 
+class GUI( qt.QWidget ):
+    def __init__( self ):
+        super( GUI, self ).__init__()
+        self.RPM = 2000
+        self.RPMplot = None
+        self.RPMData = []
+        self.RPMtext = None
+        self.RPMAxisMover = 0
+
+        self.SPEED = 25
+        self.SPEEDplot = None
+        self.SPEEDtext = None
+        self.SPEEDData = []
+        self.SPEEDAxisMover = 0
+        self.timeCounter = 0;
+
+        self.initUI()
+
+
+    def initUI( self ):
+        self.setGeometry( 0, 0, 1024, 600 )
+        self.setWindowTitle( 'Board Computer' )
+        self.setStyleSheet( "background-color:#333;" )
+        self.availableSpace =  7 * self.width() / 8
+        self.margin = self.width() / 8
+        self.addGraphs()
+        self.addTextLabels()
+        self.show()
+
+        #### self.showFullScreen() ####
+
+        self.timer = qtc.QTimer( self )
+        self.timer.setInterval( 10 )
+        self.timer.timeout.connect( self.custUpdate )
+        self.timer.start()
+
+    def custUpdate( self ):
+        self.timeCounter += 1
+
+        self.RPM += random.randint( -100 , 100 )
+        self.SPEED += random.randint( -5, 5 )
+        if self.RPM < 0: self.RPM = 0
+        if self.SPEED < 0: self.SPEED = 0
+
+
+        self.RPMtext.setText( str(self.RPM) )
+        self.SPEEDtext.setText( str( self.SPEED ) )
+
+
+        if self.timeCounter == 50:
+            self.timeCounter = 0
+            if len( self.RPMData ) < 120:
+                self.RPMData.append( self.RPM )
+            else:
+                self.RPMAxisMover += 1
+                self.RPMData[:-1] = self.RPMData[1:]
+                self.RPMData[-1] = self.RPM
+                self.RPMplot.setPos( self.RPMAxisMover, 0 )
+                self.RPMplot.setData( self.RPMData )
+                self.RPMplot.setData( self.RPMData )
+
+            self.RPMplot.setData( self.RPMData )
+
+
+            if len( self.SPEEDData ) < 120:
+               self.SPEEDData.append( self.SPEED )
+            else:
+                self.SPEEDAxisMover += 1
+                self.SPEEDData[:-1] = self.SPEEDData[1:]
+                self.SPEEDData[-1] = self.SPEED
+                self.SPEEDplot.setPos( self.SPEEDAxisMover, 0 )
+                self.SPEEDplot.setData( self.SPEEDData )
+                self.SPEEDplot.setData( self.SPEEDData )
+
+            self.SPEEDplot.setData( self.SPEEDData )
+
+
+
+
+    def paintEvent( self, e ):
+        paint = qt.QPainter()
+        paint.begin( self )
+        paint.setRenderHint( qt.QPainter.Antialiasing )
+
+        self.drawCircle( paint,  self.margin + self.availableSpace / 3, self.height() / 2, 325, 325 )
+        self.drawCircle( paint,  self.margin + 2 * self.availableSpace / 3 + 100, self.height() / 2, 325, 325 )
+
+        paint.end()
+
+
+    def drawCircle( self, paint, xCenter, yCenter, xRad, yRad, color = qt.QColor( 255, 255, 255 ) ):
+        paint.setBrush( qt.QBrush( color ) )
+        paint.drawEllipse( xCenter - xRad / 2 , yCenter - yRad / 2, xRad, yRad )
+
+
+    def addGraphs( self ):
+        self.RPMplot = pg.PlotWidget( self )
+        self.RPMplot.resize( 250, 120 )
+        self.RPMplot.move( self.margin + self.availableSpace / 3 - 125 , self.height() / 3  + 270 )
+        self.RPMplot.hideAxis( 'left' )
+        self.RPMplot.hideAxis( 'bottom' )
+        self.RPMplot = self.RPMplot.plot( self.RPMData, fillLevel = 1, brush = ( 50, 50, 200, 255 ) )
+
+        self.SPEEDplot = pg.PlotWidget( self )
+        self.SPEEDplot.resize( 250, 120 )
+        self.SPEEDplot.move( self.margin + 2 * self.availableSpace / 3 - 25 , self.height() / 3 + 270 )
+        self.SPEEDplot.hideAxis( 'left' )
+        self.SPEEDplot.hideAxis( 'bottom' )
+        self.SPEEDplot = self.SPEEDplot.plot( self.SPEEDData, fillLevel = 1, brush = ( 50, 50, 200, 255 ) )
+
+
+
+    def addTextLabels( self ):
+        fontNumber = qt.QFont( "Arial", 45, qt.QFont.Bold )
+        fontUnit = qt.QFont( "Arial", 25, qt.QFont.Bold )
+
+        self.RPMtext = qt.QLabel( "1900", self )
+        self.RPMtext.setFont( fontNumber )
+        self.RPMtext.setAlignment( qtc.Qt.AlignCenter )
+        self.RPMtext.resize( 150, 80 )
+        self.RPMtext.move( self.margin + self.availableSpace / 3 - 75, self.height() / 3 - 180 )
+
+        RPMUnit = qt.QLabel( "RPM", self )
+        RPMUnit.setFont( fontUnit )
+        RPMUnit.setAlignment( qtc.Qt.AlignCenter )
+        RPMUnit.resize( 150, 30 )
+        RPMUnit.move( self.margin + self.availableSpace / 3 - 75, self.height() / 3 - 108 )
+
+        self.SPEEDtext = qt.QLabel( "100", self )
+        self.SPEEDtext.setFont( fontNumber )
+        self.SPEEDtext.setAlignment( qtc.Qt.AlignCenter )
+        self.SPEEDtext.resize( 150, 80 )
+        self.SPEEDtext.move( self.margin + 2 * self.availableSpace / 3 + 25, self.height() / 3 - 180 )
+
+        SPEEDUnit = qt.QLabel( "km/h", self )
+        SPEEDUnit.setFont( fontUnit )
+        SPEEDUnit.setAlignment( qtc.Qt.AlignCenter )
+        SPEEDUnit.resize( 150, 30 )
+        SPEEDUnit.move( self.margin + 2 * self.availableSpace / 3 + 25, self.height() / 3 - 108 )
+
+
+
+
+
+
+
 def main():
     app = qt.QApplication( [] )
-    
-    w = qt.QWidget()
-    w.resize( 1024, 600 )
-    w.setWindowTitle( 'Board Computer' )
-    
-    button = qt.QPushButton( 'Button' )
-    text = qt.QLineEdit( 'Enter Text' )
-    listw = qt.QListWidget()
-    plot = pg.PlotWidget()
-    
-    layout = qt.QGridLayout()
-    w.setLayout( layout )
-    
-    layout.addWidget( button, 0, 0 )
-    layout.addWidget( text, 1, 0 )
-    layout.addWidget( listw, 2, 0 )
-    layout.addWidget( plot, 0, 1, 3, 1 )
-    
-    w.show()
-    
+    w = GUI()
     sys.exit( app.exec_() )
 
 
 
 if __name__ == '__main__':
+    pg.setConfigOption( 'background', ( 51, 51, 51 ) )
     main()
