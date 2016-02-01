cd31eecb538648a2e77f2efe9598f9f89400eb57
Cnidarias <pythonmolurus95@gmail.com>
2016-02-01 19:04:37 +0100

    added support for loudness rather than freq

--- sound/soundInPython/soundProcessor.py
+++ sound/soundInPython/soundProcessor.py
@@ -3,6 +3,7 @@ import os
 import time
 import threading
 import pyaudio
+import audioop
 
 from socket import *
 import json
@@ -20,18 +21,20 @@ class getSound( threading.Thread ):
     self.data = data 
     self.p = pyaudio.PyAudio()
 
+    self.rate = 12000
+
 
     self.mkHz = getHZfromSample()
 
 
     self.cs = socket( AF_INET, SOCK_DGRAM )
-    self.IP = "192.168.178.29"
+    self.IP = "192.168.178.40"
     self.PORT = 12345
 
 
     self.stream = self.p.open( format = self.p.get_format_from_width( 2 ),
         channels = 2,
-        rate = 44100,
+        rate = self.rate,
         input = True,
         output = False,
         frames_per_buffer = 512 )
@@ -62,14 +65,15 @@ class getSound( threading.Thread ):
       right = right / ( len( r ) / 2 )
 
 
-      lfreq = self.mkHz.getHz( l, 44100 )
-      rfreq = self.mkHz.getHz( r, 44100 )
+      lfreq = self.mkHz.getHz( l, self.rate )
+      rfreq = self.mkHz.getHz( r, self.rate )
 
       #self.data['leftAll'] = b64encode( l )
       #self.data['rightAll'] = b64encode( r )
 
       self.data['leftAll'] = lfreq
       self.data['rightAll'] = rfreq
+      self.data['loudness'] = audioop.rms( soundData, 2 )
       self.cs.sendto( json.dumps( self.data ), ( self.IP, self.PORT ) )
 
 
