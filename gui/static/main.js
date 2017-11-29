9d64e3dfd3a54233854fbaaebffd54cc13ddf9ae
Cnidarias <pythonmolurus95@gmail.com>
2017-11-29 23:28:32 +0100

    two graphs instead of one?

--- gui/static/main.js
+++ gui/static/main.js
@@ -4,7 +4,8 @@
 
 
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-var myNewChart;
+var rpmChart;
+var speedChart;
 
 var count = 0;
 var maxArraySize = 200;
@@ -12,14 +13,20 @@ var speedMax = 350;
 var scaleMax = 10000;
 
 
-var data = {
+var rpmData = {
     labels : ["0"],
     datasets : [
         {
             borderColor: 'rgba(20, 220, 20, 0.1)',
             backgroundColor: 'rgba(20, 220, 20, 0.5)',
             data : [0]
-        },
+        }
+    ]
+};
+
+var speedData = {
+    labels : ["0"],
+    datasets : [
         {
             borderColor: 'rgba(220, 20, 20, 0.1)',
             backgroundColor: 'rgba(220, 20, 20, 0.5)',
@@ -27,6 +34,7 @@ var data = {
         }
     ]
 };
+
 
 var options = {
     legend: {
@@ -55,6 +63,33 @@ var options = {
 };
 
 
+var options2 = {
+    legend: {
+        display: false
+    },
+    tooltips: {
+        enabled: false
+    },
+    scales: {
+        yAxes: [{
+            display:false,
+            ticks: {
+                beginAtZero: true,
+                max: speedMax
+            }
+        }],
+        xAxes: [{
+            display:false
+        }]
+    },
+    elements: {
+        point: {
+            radius: 0
+        }
+    }
+};
+
+
 socket.on('connect', function() {
     socket.emit('my event', {data: 'I\'m connected!'});
 });
@@ -64,28 +99,39 @@ socket.on('car_data', function (data) {
 });
 
 var updateChart = function(rpm, speed) {
-    if (myNewChart === null) return;
-    var labels = data["labels"];
-    var dataSet1 = data["datasets"][0]["data"];
-    var dataSet2 = data["datasets"][1]["data"];
+    if (rpmChart === null) return;
+    var labels = rpmData["labels"];
+    var labels2 = speedData["labels"];
+    var dataSet1 = rpmData["datasets"][0]["data"];
+    var dataSet2 = speedData["datasets"][0]["data"];
 
     count++;
     labels.push(count.toString());
+    labels2.push(count.toString());
     dataSet1.push(rpm);
-    dataSet2.push(speed * (scaleMax/speedMax));
+    dataSet2.push(speed);
     if (count >= maxArraySize) {
         labels.shift();
+        labels2.shift();
         dataSet1.shift();
         dataSet2.shift();
     }
-    myNewChart.update();
+    rpmChart.update();
+    speedChart.update();
 };
 
 $(document).ready(function(){
     //Get the context of the canvas element we want to select
-    $("#myChart").height($(window).height());
-    $("#myChart").width($(window).width());
-    var ctx = document.getElementById("myChart").getContext("2d");
-    myNewChart = new Chart(ctx, {type: 'line', data: data, options: options});
+    $("#rpmChart").height($(window).height()/2);
+    $("#rpmChart").width($(window).width());
+    var ctx = document.getElementById("rpmChart").getContext("2d");
+    rpmChart = new Chart(ctx, {type: 'line', data: rpmData, options: options});
+
+    $("#speedChart").height($(window).height()/2);
+    $("#speedChart").css('top', $(window).height/2);
+    $("#speedChart").width($(window).width());
+
+    var ctx = document.getElementById("speedChart").getContext("2d");
+    speedChart = new Chart(ctx, {type: 'line', data: speedData, options: options2});
 });
 
